1c0d84f979acd9520c9ddd2e7d3091575e162ba9
LomiGoo <chevellenacionales11@gmail.com>
2026-07-19 21:22:58 +0800

    database fix attempt #1

--- src/database/DatabaseManager.java
+++ src/database/DatabaseManager.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:h2:/storage/emulated/0/Android/data/classworkmanager/.database/cmli";
+    private static final String URL = "jdbc:h2:/storage/emulated/0/Android/data/com.lomigoo.classworkmanager/.database/cmli";
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, "sa", "");
