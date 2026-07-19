dc6ec2977aa836a8d051651e64f01581d72a5989
LomiGoo <chevellenacionales11@gmail.com>
2026-07-19 16:46:17 +0800

    JAR created and Modified Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import database.DatabaseInitializer;
 import menu.MainMenu;
 
+public class Main {
 void main() {
-    DatabaseInitializer.initialize();
-    AppInfo.showInfo();
-    MainMenu.show();
+        DatabaseInitializer.initialize();
+        AppInfo.showInfo();
+        MainMenu.show();
+    }
 }
